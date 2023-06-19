bb7d612d0bbcff178c9d60dc7755bd8c0dac6263
Pavel Orlov <101937419+pavelorlovyeah@users.noreply.github.com>
2023-06-19 15:51:54 +0300

    Added annotations. Corrected filter_numbers func.

--- homework_01/main.py
+++ homework_01/main.py
@@ -4,7 +4,7 @@
 """
 
 
-def power_numbers(*nums):
+def power_numbers(*nums: int) -> list:
     """
     функция, которая принимает N целых чисел,
     и возвращает список квадратов этих чисел
@@ -20,7 +20,7 @@ EVEN = "even"
 PRIME = "prime" 
 
 
-def is_prime(number):
+def is_prime(number: int) -> bool:
     if number > 1:
         for num in range(2, round(number ** 0.5) + 1):
             if number % num == 0:
@@ -28,15 +28,15 @@ def is_prime(number):
         return True
 
 
-def is_odd(number):
+def is_odd(number: int) -> bool:
     return number % 2 != 0
 
 
-def is_even(number):
+def is_even(number: int) -> bool:
     return number % 2 == 0
 
 
-def filter_numbers(numbers, numbers_type):
+def filter_numbers(numbers: list, numbers_type: str) -> list:
     """
     функция, которая на вход принимает список из целых чисел,
     и возвращает только чётные/нечётные/простые числа
@@ -47,6 +47,9 @@ def filter_numbers(numbers, numbers_type):
     >>> filter_numbers([2, 3, 4, 5], EVEN)
     <<< [2, 4]
     """
+    if numbers_type not in [EVEN, ODD, PRIME]:
+        print('Incorrect additional argument')
+        return []
     if numbers_type == EVEN:
         return list(filter(is_even, numbers))
     if numbers_type == ODD:
